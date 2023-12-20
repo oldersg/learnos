29fe7e6abf0c0ddfb014261231d2ea4b60326200
hello <email@example.com>
2023-12-20 20:35:00 +0800

    chapter7 ends

--- chapter7/001/kernel/interrupt.c
+++ chapter7/001/kernel/interrupt.c
@@ -42,7 +42,7 @@ static void pic_init(void) {
     outb (PIC_S_DATA, 0x01);	// ICW4: 8086模式, 正常EOI
 
     /* 打开主片上IR0,也就是目前只接受时钟产生的中断 */
-    outb (PIC_M_DATA, 0xfe);
+    outb (PIC_M_DATA, 0xfe);    //此时使用的是OCW1
     outb (PIC_S_DATA, 0xff);
 
     put_str("   pic_init done\n");
